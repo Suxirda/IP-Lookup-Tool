19582b0592d70a347ed1dd5332d26409f02d582e
Suxirda <148821696+Suxirda@users.noreply.github.com>
2024-01-26 14:05:01 -0800

    Add files via upload

--- install_python.bat
+++ install_python.bat
@@ -1,17 +1,22 @@
 @echo off
-title Downloading Python...
 set PYTHON_VERSION=3.12.1
 set PYTHON_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe
-set DOWNLOAD_PATH=python-%PYTHON_VERSION%-amd64.exe
+set INSTALLER_NAME=python-installer.exe
+set INSTALL_DIR=C:\Python
 
-echo Downloading Python %PYTHON_VERSION%...
-curl -o %DOWNLOAD_PATH% -L %PYTHON_URL%
+echo Downloading Python installer...
+curl -o %INSTALLER_NAME% %PYTHON_URL%
 
-echo Installing Python %PYTHON_VERSION%...
-start /wait %DOWNLOAD_PATH% /quiet InstallAllUsers=1 PrependPath=1
+echo Installing Python...
+start /wait %INSTALLER_NAME% /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+echo Adding Python to system path...
+setx PATH "%PATH%;%INSTALL_DIR%\;%INSTALL_DIR%\Scripts\" /M
 
 echo Cleaning up...
-title Cleaning Up...
-del %DOWNLOAD_PATH%
+del %INSTALLER_NAME%
 
-echo Python %PYTHON_VERSION% has been installed.
+echo Python %PYTHON_VERSION% is installed and added to the system path.
+echo Please restart your command prompt to use Python.
+
+pause
